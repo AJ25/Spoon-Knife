e27f6ff51e4fdbf1d951fb7204251648bdf8ec68
Ajay Jadhav <ajayjadhav@yahoo-inc.com>
2014-04-03 20:03:48 -0700

    Adding a new file

---
+++ wix.js
@@ -0,0 +1,24 @@
+var siteTitle;
+var referrer;
+var url;
+
+Wix.getSiteInfo(function onSuccess(siteInfo) {
+
+	siteTitle= siteInfo.siteTitle;
+	referrer= siteInfo.referrer;
+	url= siteInfo.url;
+});
+
+$.w._ycc.__internal.api= {
+
+	getPageTitle: function() {
+		return siteTitle;
+	}, 
+	getReferer: function() {
+		return referrer;
+	}, 
+	getUrl: function() {
+		return url;
+	} 
+};
+
